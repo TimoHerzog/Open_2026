71396075050484ac8031bdc19a29657a6016cc6e
W4LL3-UZUM4K1 <mrius.myr@gmail.com>
2026-04-10 18:09:54 +0200

    Test CI pipeline with failing unit tests

--- test_main.py
+++ test_main.py
@@ -5,9 +5,9 @@ from main import Verkaufte_Menge, Ausgabe
 def test_berechnung_logik():
     # Beispiel: Preis 2€, Einnahmen 20€ -> Menge 10
     m1, m2, m3 = Verkaufte_Menge(2, 2, 2, 20, 40, 60)
-    assert m1 == 10
-    assert m2 == 20
-    assert m3 == 30
+    assert m1 == 100
+    assert m2 == 100
+    assert m3 == 100
 
 # Testet die Textausgabe für den Gewinner
 def test_ausgabe_sieger(capsys):
